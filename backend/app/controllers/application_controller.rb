e50970309aec8237c56dbebcfc92a2bf256785cb
Victor Turitzin <turitzinv@gmail.com>
2022-05-31 15:52:26 -0400

    Added a get for libraries and books in application_controller.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  #get "/" do
+  #  { message: "Good luck with your project!" }.to_json
+  #end
+
+  #Select drop down to see all libraries? 
+  #Or general page showing all libraries and books?
+  get '/libraries' do
+    libraries = Library.all
+    libraries.to_json
   end
+
+  get '/books' do
+    books = Book.all
+    books.to_json
+  end
+  
 
 end
