8cddf9fb02d1a9ec2ced701eb6bd7531037c7f8d
Victor Turitzin <turitzinv@gmail.com>
2022-06-01 11:25:09 -0400

    Added to seeds to have sample database.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_31_184053) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "year"
+    t.boolean "read"
+    t.integer "library_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,57 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding Libraries and Books..."
 
 # Seed your database here
 
-#captain_america = Book.find(1)
+Library.create(genre: "Comic Books")
+Library.create(genre: "Adventure")
+Library.create(genre: "How To")
+
+Book.create(
+  title: "Captain American Comics #1",
+  author: "Joe Simon, Jack Kirby",
+  year: 1941,
+  read: true,
+  library_id: 1
+)
+
+Book.create(
+  title: "Iron Man",
+  author: "Archie Goodwin",
+  year: 1968,
+  read: false,
+  library_id: 1
+)
+
+Book.create(
+  title: "Treasure Island",
+  author: "Robert Louis Stevenson",
+  year: 1883,
+  read: true,
+  library_id: 2
+)
+
+Book.create(
+  title: "Moby Dick",
+  author: "Herman Melville",
+  year: 1851,
+  read: true,
+  library_id: 2
+)
+
+Book.create(
+  title: "The Book of Ruby",
+  author: "Huw Collingbourne",
+  year: 2011,
+  read: true,
+  library_id: 3
+)
+
+Book.create(
+  title: "Origami 365",
+  author: "Taro Yaguchi",
+  year: 2014,
+  read: false,
+  library_id: 3
+)
 
 puts "✅ Done seeding!"
